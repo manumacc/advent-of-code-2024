f677f6b0376e7029fbd2b7c6dabfa4e72be42a62
Manuele Macchia <macchiamanuele@gmail.com>
2024-12-01 11:04:53 +0100

    Solve day 1

---
+++ 01/solution.py
@@ -0,0 +1,23 @@
+# first star
+with open("input.txt", "r") as f:
+    pairs = [l.strip().split("   ") for l in f.readlines()]
+
+left = [int(l) for l, r in pairs]
+right = [int(r) for l, r in pairs]
+
+distances = [abs(l - r) for l, r in zip(sorted(left), sorted(right))]
+
+total_distance = sum(distances)
+print(total_distance)
+
+# second distance
+lookup = {}
+for r in sorted(right):
+    if r in lookup:
+        lookup[r] += 1
+    else:
+        lookup[r] = 1
+
+similarities = [l * lookup.get(l, 0) for l in left]
+total_similarities = sum(similarities)
+print(total_similarities)
